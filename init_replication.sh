280ca856225b6dae506fca8d61927214da2a0bad
gaurav.jain.1582@gmail.com <gaurav.jain.1582@gmail.com>
2018-08-23 17:47:01 -0700

    oush t

--- init_replication.sh
+++ init_replication.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=mongoDBStack_mdbS1) mongo --eval 'rs.initiate({ _id: "mdb_repSet", members: [{ _id: 1, host: "mongoDBStack_mdbS1:27017" }, { _id: 2, host: "mongoDBStack_mdbS2:27017" },{ _id: 3, host: "mongoDBStack_mdbS3:27017" }], settings: { getLastErrorDefaults: { w: "majority", wtimeout: 30000 }}})'
+#docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=mongoDBStack_mdbS1) mongo --eval 'rs.initiate({ _id: "mdb_repSet", members: [{ _id: 1, host: "mongoDBStack_mdbS1:27017" }, { _id: 2, host: "mongoDBStack_mdbS2:27017" },{ _id: 3, host: "mongoDBStack_mdbS3:27017" }], settings: { getLastErrorDefaults: { w: "majority", wtimeout: 30000 }}})'
 
+docker exec -it 6599775ab18d  mongo --eval 'rs.initiate({ _id: "mdb_repSet", members: [{ _id: 1, host: "mongoDBStack_mdbS1:27017" }, { _id: 2, host: "mongoDBStack_mdbS2:27017" },{ _id: 3, host: "mongoDBStack_mdbS3:27017" }], settings: { getLastErrorDefaults: { w: "majority", wtimeout: 30000 }}})'
 #docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=mongoDBStack_mdbS1) mongo --eval 'rs.config()'
 #docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=mongoDBStack_mdbS1) mongo --eval 'rs.status()'
 #docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=mongoDBStack_mdbS1) mongo --eval 'rs.printReplicationInfo()'
